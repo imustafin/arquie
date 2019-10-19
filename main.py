b94c4fdb97cefd4d77cd42b5950928accaf0c09a
anjas29 <anjasmoro.adi29@gmail.com>
2019-10-19 15:26:24 +0300

    Query function

--- main.py
+++ main.py
@@ -16,8 +16,66 @@ def print_duplicate_name_warnings():
 def print_warnings():
     print_duplicate_name_warnings()
 
+def query():
+	user_query = input ("\nInput your query: ")
+	query_parse(user_query)
+
+def query_parse(user_query):
+	if "NODES" in user_query.upper():
+		query_node_parse()
+	elif "RELATION" in user_query.upper():
+		query_relation_parse()
+	# elif "NODES NO-RELATION" in user_query.upper():
+	# 	print("NR!")
+	else:
+		print("Query is not recognized!")
+
+def query_node_parse():
+	class_name = input("Input Class Name: ")
+	query_execution = "MATCH (n {class:'"+class_name+"'}) return n"
+
+	print("\nList of Nodes of "+class_name+":")
+
+	total_data = 0
+	for r in g.run(query_execution):
+		n = r['n']
+		print(f'{format_node(n)}')
+		total_data += 1
+
+	if total_data < 1:
+		print("There is no data")
+
+def query_relation_parse():
+	name = input("Input Element Name: ")
+	limit = input("(Optional) Input maximum depth level integer: ")
+	class_name = input("(Optional) Input specific class type: ")
+
+	if limit == "":
+		limit_query = 10
+	else:
+		limit_query = limit
+
+	if class_name == "":
+		query_execution = "MATCH (n)<-[*1.."+str(limit_query)+"]-(c{name:'"+name+"'}) RETURN c, n"
+	else:
+		query_execution = "MATCH (n {class:'"+class_name+"'})<-[*1.."+str(limit_query)+"]-(c{name:'"+name+"'}) RETURN c, n"
+
+	print("\nList of connected Nodes from "+name+":")
+
+	total_data = 0
+	for r in g.run(query_execution):
+		c, n = r['c'], r['n']
+		print(f'{format_node(n)}')
+		total_data += 1
+
+	if total_data < 1:
+		print("There is no data")
+
 def main():
     print_warnings()
+   
+    while(True):
+    	query()
 
 if __name__ == '__main__':
     main()
