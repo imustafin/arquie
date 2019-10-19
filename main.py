fba210233068c688ec480da4bb4d983b3188ad3e
Ilgiz Mustafin <ilgimustafin@gmail.com>
2019-10-19 12:42:51 +0300

    Add duplicate name warning

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ from py2neo import Graph
 
 g = Graph(user='neo4j', password='123')
 
-print('Hi! You have these nodes in your db:')
+def format_node(n):
+    return '#' + str(n['id']) + ' ' + n['class'] + ' "' + n['name'] + '"'
 
-for r in g.run('MATCH (n) RETURN n'):
-    n = r['n']
-    print(dict(n))
+def print_duplicate_name_warnings():
+    for r in g.run(
+            'MATCH (n), (m) WHERE n.name = m.name '
+            'AND n <> m AND ID(n) < ID(m) RETURN n, m'):
+        n, m = r['n'], r['m']
+        print(f'Duplicate name: {format_node(n)} and {format_node(m)} '
+              'have the same name')
+
+def print_warnings():
+    print_duplicate_name_warnings()
+
+def main():
+    print_warnings()
+
+if __name__ == '__main__':
+    main()
